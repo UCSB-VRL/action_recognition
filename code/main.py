2c2fe3311b338ef7e1a98cf640904253cb3c02f3
AJB <archithjohn247@gmail.com>
2017-03-12 17:27:19 -0700

    1. Added precision-1 and precision-5 measurement code 2. Added checkpoint save code 3. Added code for weight-decay regularization

--- code/main.py
+++ code/main.py
@@ -48,6 +48,12 @@ parser.add_argument('--operation_mode', default='feature', dest='operation_mode'
 parser.add_argument('--feature_root', default='', dest='feature_root',
                     help='If "feature_mode" is set to "feature", then pre-computed features are saved under location  \
                           "feature_root"')
+parser.add_argument('--save', type=str,  dest='save', default='lstm_saved_model.pt',
+                    help='path to save the final model')
+parser.add_argument('--momentum', default=0.9, type=float, dest='momentum',
+                    help='momentum weight')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    dest='weight_decay', help='weight decay (default: 1e-4)')
 
 args = parser.parse_args()
 
@@ -55,7 +61,7 @@ args = parser.parse_args()
 torch.manual_seed(args.seed)
 torch.cuda.manual_seed(args.seed)
 
-
+print os.environ['CUDA_VISIBLE_DEVICES']
 feat_size_map = { 'vgg16': 4096,
                   'resnet152': 2048
                 }
@@ -83,7 +89,6 @@ assert args.operation_mode in ['feature', 'image']
 print 'Mode of operation : %s'%args.operation_mode.upper()
 if args.operation_mode == 'feature':
   # Precompute features from sequence images and store on disk for later processing as LSTM inputs
-  curr_seq_featurefiles = glob.glob(os.path.join(args.feature_root, '*.npy'))
   split_names = ['train', 'test', 'val']
   split_nameidx_map = {}
   for idx, x in enumerate(split_names):
@@ -120,7 +125,7 @@ if args.operation_mode == 'feature':
       seq_features = seq_features.view(seq_len, bz, -1)
 
       for b_index in range(bz):
-        seq_feature_filename = os.path.join(seq_file_folder, seq_file_format%seq_feature_counter)
+        seq_feature_filename = seq_file_format%seq_feature_counter
         np.save(seq_feature_filename, seq_features.narrow(1, b_index, 1).squeeze().data.cpu().numpy())
         seq_feature_filenames.append(seq_feature_filename)
         seq_feature_labels.append(label[b_index])
@@ -158,6 +163,26 @@ lstm_model = lstm.RNNModel('LSTM', feat_size_map[args.feature_extractortype], ar
 lstm_model.cuda()
 
 lstm_criterion = nn.CrossEntropyLoss()
+lstm_optimizer = torch.optim.SGD(lstm_model.parameters(), args.lr,
+                            momentum=args.momentum,
+                            weight_decay=args.weight_decay)
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
 
 def repackage_hidden(h):
   """Wraps hidden states in new Variables, to detach them from their history."""
@@ -167,7 +192,7 @@ def repackage_hidden(h):
     return tuple(repackage_hidden(v) for v in h)
 
 def train():
-  total_loss = 0
+  total_loss = 0; total_prec1 = 0; total_prec5 = 0
   start_time = time.time()
   zero_batch_time = time.time()
   hidden = lstm_model.init_hidden(args.batch_size)
@@ -194,11 +219,14 @@ def train():
       lstm_input = Variable(lstm_input.cuda(), volatile=False)
 
     hidden = repackage_hidden(hidden)
-    lstm_model.zero_grad()
+    lstm_optimizer.zero_grad()
     output, hidden = lstm_model(lstm_input, hidden)
     loss = lstm_criterion(output.view(-1, dataset.get_numclasses()),
                           lstm_label)
+    train_prec1, train_prec5 = accuracy(output.view(-1, dataset.get_numclasses()).data, lstm_label.data, topk=(1, 5))
+    total_prec1 += train_prec1[0]; total_prec5 += train_prec5[0]
     loss.backward()
+    lstm_optimizer.step()
 
     for p in lstm_model.parameters():
       p.data.add_(-lr, p.grad.data)
@@ -207,17 +235,21 @@ def train():
 
     if batch_idx % args.log_interval == 0 and batch_idx > 0:
       cur_loss = total_loss[0] / args.log_interval
+      curr_prec1 = total_prec1/ args.log_interval; curr_prec5 = total_prec5/ args.log_interval
       elapsed = time.time() - start_time
       print('elapsed_time : {} | epoch {:3d} | {:6d}/{:6d} batches | lr {:02.6f} | ms/batch {:8.2f} | '
-            'loss {:5.2f} | ppl {:8.2f}'.format(str(datetime.timedelta(seconds=time.time()-zero_batch_time)), epoch, batch_idx, len(train_data) // args.batch_size, lr,
-                      elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
+            'loss {:5.2f} | top1 {:8.2f} | top5 {:8.2f}'.format(str(datetime.timedelta(seconds=time.time()-zero_batch_time)), epoch, batch_idx, len(train_data) // args.batch_size, lr,
+                      elapsed * 1000 / args.log_interval, cur_loss, curr_prec1, curr_prec5))
       total_loss = 0
+      total_prec1 = 0
+      total_prec5 = 0
       start_time = time.time()
 
 def evaluate(loader):
   total_loss = 0
   hidden = lstm_model.init_hidden(args.batch_size)
   num_batchesprocessed = 0
+  total_prec1 = 0; total_prec5 = 0
   for batch_idx, (eval_seq, eval_label, _) in enumerate(loader):
     num_batchsamples = eval_seq.size(0)  # Hack, to avoid running into incomplete batches
     if num_batchsamples < args.batch_size:
@@ -241,26 +273,58 @@ def evaluate(loader):
 
     output, hidden = lstm_model(lstm_input, hidden)
     output_flat = output.view(-1, dataset.get_numclasses())
+    eval_prec1, eval_prec5 = accuracy(output_flat.data, lstm_label.data, topk=(1, 5))
+    total_prec1 += eval_prec1[0]; total_prec5 += eval_prec5[0]
+
     total_loss += len(eval_seq) * lstm_criterion(output_flat, lstm_label).data
     hidden = repackage_hidden(hidden)
     num_batchesprocessed += 1
 
-  return total_loss[0] / ((num_batchesprocessed)*args.batch_size)
+  return total_loss[0] / ((num_batchesprocessed)*args.batch_size), total_prec1/num_batchesprocessed, total_prec5/num_batchesprocessed
+
+def adjust_learning_rate(optimizer, lr):
+  """Sets the learning rate to the initial LR decayed by 10"""
+  lr = lr*0.1
+  for param_group in optimizer.param_groups:
+    param_group['lr'] = lr
+  return lr
 
 # Loop over epochs.
 lr = args.lr
-prev_val_loss = None
-for epoch in range(1, args.epochs+1):
-    epoch_start_time = time.time()
-    train()
-    val_loss = evaluate(valdata_loader)
-    print 'Finised Eval'
+best_val_loss = None
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+  prev_val_loss = None
+  for epoch in range(1, args.epochs+1):
+      epoch_start_time = time.time()
+      train()
+      val_loss, val_top1, val_top5 = evaluate(valdata_loader)
+      print 'Finised Eval'
+      print('-' * 89)
+      print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+              'top1 {:8.2f} | top5 {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                         val_loss, val_top1, val_top5))
+      print('-' * 89)
+      # Save the model if the validation loss is the best we've seen so far.
+      if not best_val_loss or val_loss < best_val_loss:
+        with open(args.save, 'wb') as f:
+          torch.save(lstm_model, f)
+        best_val_loss = val_loss
+      # Anneal the learning rate if no improvement has been seen in the validation dataset.
+      else:
+        lr = adjust_learning_rate(lstm_criterion, lr)
+
+except KeyboardInterrupt:
     print('-' * 89)
-    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-            'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                       val_loss, math.exp(val_loss)))
-    print('-' * 89)
-    # Anneal the learning rate.
-    if prev_val_loss and val_loss > prev_val_loss:
-        lr /= 4
-    prev_val_loss = val_loss
+    print('Exiting from training early')
+
+# Load the best saved model.
+with open(args.save, 'rb') as f:
+  lstm_model = torch.load(f)
+
+# Run on test data.
+test_loss, test_top1, test_top5  = evaluate(testdata_loader)
+print('=' * 89)
+print('| End of training | test loss {:5.2f} | top1 {:8.2f} | top5 {:8.2f}'.format(
+    test_loss, test_top1, test_top5))
+print('=' * 89)
